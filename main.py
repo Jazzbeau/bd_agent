dd78dd52cd18f506aaec522267c3d9026b6b40a9
jazz <jazz@jazzbe.au>
2026-01-04 19:21:03 +1030

    separate concerns of response handling, add verbosity to argument parameters

--- main.py
+++ main.py
@@ -13,40 +13,46 @@ if API_KEY is None:
     raise RuntimeError("'GEMINI_API_KEY' environment variable not found'")
 
 
-def make_parser():
+def get_parser():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     return parser
 
 
 def get_response(messages):
+    # generate_content accepts the list of Content instances; sends GenerateContentResponse back.
     response = CLIENT.models.generate_content(model=MODEL, contents=messages)
     if response.usage_metadata is None:
         raise RuntimeError("API query failure")
-
-    return (
-        response.text,
-        response.usage_metadata.prompt_token_count,
-        response.usage_metadata.candidates_token_count,
-    )
+    return response
 
 
-def print_verbose_response(
-    query, response_text, prompt_token_count, candidate_token_count
-):
-    print(
-        f"User prompt:{query}\nPrompt tokens: {prompt_token_count}\nResponse tokens: {candidate_token_count}\nResponse:\n{response_text}"
-    )
+def print_response(query, response, verbose):
+    response_text = response.text
+    if verbose:
+        prompt_token_count = response.usage_metadata.prompt_token_count
+        candidates_token_count = response.usage_metadata.candidates_token_count
+        print(
+            f"User prompt:{query}\nPrompt tokens: {prompt_token_count}\nResponse tokens: {candidates_token_count}\nResponse:\n{response_text}"
+        )
+    else:
+        print(response_text)
 
 
 def main():
-    parser = make_parser()
+    # Setup
+    parser = get_parser()
     args = parser.parse_args()
-    # A list of messages, which take the form of a `types.Content` instance, containing a role, and in our case, a series of
-    # text parts
     query = args.user_prompt
+
+    # List of `genai.types.Content` instance, each contains role + list of parts (text)
     messages = [types.Content(role="user", parts=[types.Part(text=query)])]
-    print_verbose_response(query, *get_response(messages))
+
+    # Error and other logic wrapper over generate_content
+    response = get_response(messages)
+
+    print_response(query, response, args.verbose)
 
 
 if __name__ == "__main__":
