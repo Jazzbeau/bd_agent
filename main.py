96879f8f72c6906ea2d97b64bacd4c75be63bb9e
jazz <jazz@jazzbe.au>
2026-01-04 17:03:48 +1030

    enable user input

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
@@ -7,12 +8,17 @@ load_dotenv()
 API_KEY = os.environ.get("GEMINI_API_KEY")
 CLIENT = genai.Client(api_key=API_KEY)
 MODEL = "gemini-2.5-flash"
-
 if API_KEY is None:
     raise RuntimeError("'GEMINI_API_KEY' environment variable not found'")
 
 
-def getResponse(query):
+def make_parser():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    return parser
+
+
+def get_response(query):
     response = CLIENT.models.generate_content(model=MODEL, contents=query)
     if response.usage_metadata is None:
         raise RuntimeError("API query failure")
@@ -24,7 +30,7 @@ def getResponse(query):
     )
 
 
-def printVerboseResponse(
+def print_verbose_response(
     query, response_text, prompt_token_count, candidate_token_count
 ):
     print(
@@ -33,8 +39,10 @@ def printVerboseResponse(
 
 
 def main():
-    query = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    printVerboseResponse(query, *getResponse(query))
+    parser = make_parser()
+    args = parser.parse_args()
+    query = args.user_prompt
+    print_verbose_response(query, *get_response(query))
 
 
 if __name__ == "__main__":
