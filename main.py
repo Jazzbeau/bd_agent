bb6794d391edb50da79e4ea42807b317976c3fcc
jazz <jazz@jazzbe.au>
2026-01-04 13:47:15 +1030

    add dotenv import and check

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key is None:
+    raise RuntimeError("'GEMINI_API_KEY' environment variable not found'")
+
+
 def main():
     print("Hello from ai-agent!")
 
