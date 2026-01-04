c46dec93831947446425df76da1fc17e998fd00d
jazz <jazz@jazzbe.au>
2026-01-04 16:11:32 +1030

    refactor into functions, add verbose output

--- main.py
+++ main.py
@@ -12,11 +12,29 @@ if API_KEY is None:
     raise RuntimeError("'GEMINI_API_KEY' environment variable not found'")
 
 
-def main():
-    response = CLIENT.models.generate_content(
-        model=MODEL, contents="Does anyone expect the Spanish Inquisition?"
+def getResponse(query):
+    response = CLIENT.models.generate_content(model=MODEL, contents=query)
+    if response.usage_metadata is None:
+        raise RuntimeError("API query failure")
+
+    return (
+        response.text,
+        response.usage_metadata.prompt_token_count,
+        response.usage_metadata.candidates_token_count,
     )
-    print(response.text)
+
+
+def printVerboseResponse(
+    query, response_text, prompt_token_count, candidate_token_count
+):
+    print(
+        f"User prompt:{query}\nPrompt tokens: {prompt_token_count}\nResponse tokens: {candidate_token_count}\nResponse:\n{response_text}"
+    )
+
+
+def main():
+    query = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    printVerboseResponse(query, *getResponse(query))
 
 
 if __name__ == "__main__":
