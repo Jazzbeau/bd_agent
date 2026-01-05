6ee41f5fe584ff945db20c476f965793e40efbf5
jazz <jazz@jazzbe.au>
2026-01-06 01:26:46 +1030

    fix file path logic

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,17 +5,19 @@ import config
 
 def get_file_content(working_directory, file_path):
     try:
-        if os.path.commonpath([working_directory, file_path]) != working_directory:
+        abs_dir = os.path.abspath(working_directory)
+        abs_file = os.path.normpath(os.path.join(abs_dir, file_path))
+        if os.path.commonpath([abs_dir, abs_file]) != abs_dir:
             raise ValueError(
                 f'Cannot read "{file_path}" as it is outside the permitted working directory'
             )
-        if not os.path.isfile(file_path):
+        if not os.path.isfile(abs_file):
             raise ValueError(
                 f'Error: File not found or is not a regular file: "{file_path}"'
             )
 
         # Read from file
-        with open(file_path, "r") as f:
+        with open(abs_file, "r") as f:
             file_content_string = f.read(config.MAX_CHARS)
             # If file returns true - not empty. Signify truncation
             if f.read(1):
