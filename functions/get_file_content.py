39e092db5ec0be15683b0c3bf6443242440a996b
jazz <jazz@jazzbe.au>
2026-01-06 01:17:04 +1030

    add get_file_content
    contains function to fetch N characters (as defined by config.MAX_CHARS)
    from specified file path, with working directory bounds checking

---
+++ functions/get_file_content.py
@@ -0,0 +1,24 @@
+import os
+
+import config
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        if os.path.commonpath([working_directory, file_path]) != working_directory:
+            raise ValueError(
+                f'Cannot read "{file_path}" as it is outside the permitted working directory'
+            )
+        if not os.path.isfile(file_path):
+            raise ValueError(
+                f'Error: File not found or is not a regular file: "{file_path}"'
+            )
+
+        # Read from file
+        with open(file_path, "r") as f:
+            file_content_string = f.read(config.MAX_CHARS)
+            # If file returns true - not empty. Signify truncation
+            if f.read(1):
+                file_content_string += f'[...File "{file_path}" truncated at {config.MAX_CHARS} characters]'
+    except Exception as e:
+        return f"Error: {e}"
